087270f3984015da7eeb7ae16abb66f1e733d23b
iLyas Bakouch <isbkch@gmail.com>
2024-02-12 11:55:19 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,10 @@
 import boto3
 from datetime import datetime, timedelta, timezone
 
-# CHANGE THESE VARIABLES
-region_name = 'YOUR_REGION_NAME'
-account_id = 'YOUR_ACCOUNT_ID'
-state_machine_name = 'YOUR_STATE_MACHINE_NAME'
-
+# Your AWS region
+region_name = 'us-east-1'
 # ARN of the Step Functions state machine
-state_machine_arn = 'arn:aws:states:{}:{}:stateMachine:{}'.format(region_name, account_id, state_machine_name)
+state_machine_arn = 'arn:aws:states:us-east-1:524744845066:stateMachine:EventsWrangler'
 
 # Create a Step Functions client
 client = boto3.client('stepfunctions', region_name=region_name)
@@ -47,27 +44,14 @@ def redrive_failed_executions(executions):
     """Start a new execution for each failed execution provided."""
     for execution in executions:
         try:
-            exec_desc = client.describe_execution(executionArn=execution['executionArn'])
-            original_input = exec_desc['input']
+            failedExecutionArn = execution['executionArn']
             
-            # Check if the input starts with {"ExecutionArn":
-            if original_input.strip().startswith('{"ExecutionArn":'):
-                print(f"Skipping execution {execution['executionArn']} due to input filter.")
-                continue
-            
-            # Ensure the original_input is correctly parsed as a string if needed
-            if isinstance(original_input, dict):
-                import json
-                original_input = json.dumps(original_input)
-            
-            print(f"Starting new execution of {execution['executionArn']} using  {original_input}")
-            response = client.start_execution(
-                stateMachineArn=state_machine_arn,
-                input=original_input
+            print(f"Starting new execution of {failedExecutionArn}")
+            client.redrive_execution(
+                executionArn=failedExecutionArn
             )
-            print (f"New execution response: {response['executionArn']}")
         except Exception as e:
-            print(f"Error redriving execution {execution['executionArn']}: {e}")
+            print(f"Error redriving execution {failedExecutionArn} => {e}")
 
 if __name__ == "__main__":
     # Calculate the timestamp for 24 hours ago in UTC
